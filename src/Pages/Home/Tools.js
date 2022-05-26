db6e17813efd3444380f2a5b3192609184c319e4
bilkis01 <bilkiscse@gmail.com>
2022-05-26 19:09:22 +0600

    shop deploy

--- src/Pages/Home/Tools.js
+++ src/Pages/Home/Tools.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 
-const Tools = ({ tool, setPurchase }) => {
+const Tools = ({ tool }) => {
     const { name, img, available,minimum, Description, price, _id } = tool;
     return (
         <div>
@@ -11,13 +11,13 @@ const Tools = ({ tool, setPurchase }) => {
                 <div className="card-body">
                     <h2 className="card-title">{name}</h2>
                     <p>{Description}</p>
-                    <p className='font-bold text-purple-400'>Available quantity : {available}</p>
-                    <p className='font-bold text-base-400'>Minium : {minimum}</p>
+                    <p className='font-bold text-green-400'>Available quantity : {available}</p>
+                    <p className='font-bold text-green-300'>Minium : {minimum}</p>
                     <p className='font-bold'>${price}</p>
                     
                     <div className="card-actions justify-end">
                         
-                    <Link className='btn btn-primary' to={`/shop/${_id}`}>Purchase</Link>
+                    <Link className='btn bg-green-400 text-white' to={`/shop/${_id}`}>Purchase</Link>
                     </div>
                 </div>
             </div>
